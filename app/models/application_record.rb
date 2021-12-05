6f7189d6d7a7a024bd32c54a90b5dac6ad4abc33
Danielle Thompson <danithompson74@gmail.com>
2021-12-05 14:19:18 -0800

    Add notes explaining `self.abstract_class = true`
    and its relationship to the db

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,6 @@
 # ActiveRecord::Base automatically creates all CRUD functionality
 class ApplicationRecord < ActiveRecord::Base
+  # Tells the database not to ever look for ApplicationRecord as a table
+  # Read: https://medium.com/@jeremy_96642/deep-rails-how-to-use-abstract-classes-6aee9b686e75
   self.abstract_class = true
 end
