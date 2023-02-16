a71fd7b51942ae01a585b6bd339da0c59705c1b7
Danielle Thompson <danithompson74@gmail.com>
2023-02-15 18:45:25 -0800

    Guarantee [referential integrity](https://guides.rubyonrails.org/active_record_migrations.html#active-record-and-referential-integrity)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_024132) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_024333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_024132) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "songs", "albums"
 end
