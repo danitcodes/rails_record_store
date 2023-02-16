7bf171dd8dc0390da8c72148f425442365777a90
Danielle Thompson <danithompson74@gmail.com>
2023-02-15 18:43:15 -0800

    Create songs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_020907) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_024132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_020907) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.string "lyricsc"
+    t.integer "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
