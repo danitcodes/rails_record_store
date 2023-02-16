efab17e2601385803107bf5c741292ff76efd87a
Danielle Thompson <danithompson74@gmail.com>
2023-02-15 19:07:54 -0800

    Add CRUD fxnality to `album`

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,12 @@
+# albums GET      /albums(.:format)                albums#index
+# POST            /albums(.:format)                albums#create
+# new_album GET   /albums/new(.:format)            albums#new
+# edit_album GET  /albums/:id/edit(.:format)       albums#edit
+# album GET       /albums/:id(.:format)            albums#show
+# PATCH           /albums/:id(.:format)            albums#update
+# PUT             /albums/:id(.:format)            albums#update
+# DELETE          /albums/:id(.:format)
+
 class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
 end
